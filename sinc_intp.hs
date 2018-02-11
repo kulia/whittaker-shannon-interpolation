095eb57b0f820c423aa344bd36c54ce4a9750236
Geir Kulia <geir@kulia.no>
2018-02-11 22:59:40 +0100

    Fix sampling frequency of signal

--- sinc_intp.hs
+++ sinc_intp.hs
@@ -13,6 +13,7 @@ data DiscreteSignal = DiscreteSignal SamplingPeriod [Sample]
 signal :: SignalFunc
 signal x = (sin (x*pi/45) + 1) / 2 * (sin (x*pi/5))
 
+ts = 2.3
 
 resample :: DiscreteSignal -> SignalFunc
 resample signal =
@@ -23,17 +24,18 @@ resample signal =
      bases t = [sinc $ (t - n*ts)/ts | n <- [0..]]
 
 sampledSignal :: DiscreteSignal
-sampledSignal = DiscreteSignal 7 $ map signal [0,7..400]
+sampledSignal = DiscreteSignal ts $ map signal [0,ts..400]
 
 resampledSignal :: SignalFunc
 resampledSignal = resample sampledSignal
 
 main = toFile def "fig/example_resample.svg" $ do
   layout_title .= "Resample Signal"
-  setColors [opaque blue, opaque red]
+  setColors [opaque green ,opaque blue, opaque red]
   -- plot (line "Sinc" [])
-  plot (line "Signal" [(map (\x -> (x, resampledSignal x)) [0,(0.5)..400])])
-  plot (points "Discrete Time" (zip [0,7..400] samples))
+  plot (line "Signal" [(map (\x -> (x, signal x)) [0,(0.5)..400])])
+  plot (line "Reconstructed Signal" [(map (\x -> (x, resampledSignal x)) [0,(0.5)..400])])
+  plot (points "Discrete Time" (zip [0,ts..400] samples))
   where
     DiscreteSignal _ samples = sampledSignal
 
